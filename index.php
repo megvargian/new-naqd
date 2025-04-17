fde0a8e365d139e7a5a3a508a0bb69ab5850df21
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-04-17 16:25:06 +0300

    test

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ get_header();
             <?php for($i=0; $i<24; $i++){ ?>
                 <div class="col-lg-3 col-12 mb-2">
                     <a href="#">
-                        <img class="w-100 d-block single-article" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/berry.jpg" alt="berry">
+                        <img class="w-100 d-block single-article px-1" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/berry.jpg" alt="berry">
                     </a>
                 </div>
             <?php } ?>
