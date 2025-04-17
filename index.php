73ef6c95a062e9bb43b6808f15ad0531ea565324
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-04-17 16:27:02 +0300

    test

--- index.php
+++ index.php
@@ -8,12 +8,15 @@ get_header();
 // endwhile;
 ?>
 <section class="homepage">
-    <div class="container">
-        <div class="row py-5">
-            <?php for($i=0; $i<24; $i++){ ?>
-                <div class="col-lg-3 col-12 mb-2">
+    <div class="container py-5">
+        <div class="row">
+
+        </div>
+        <div class="row">
+            <?php for($i=0; $i<12; $i++){ ?>
+                <div class="col-lg-3 col-12 mb-2 px-2">
                     <a href="#">
-                        <img class="w-100 d-block single-article px-1" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/berry.jpg" alt="berry">
+                        <img class="w-100 d-block single-article " src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/berry.jpg" alt="berry">
                     </a>
                 </div>
             <?php } ?>
