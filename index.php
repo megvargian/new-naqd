f4dbd1b2bce6b4081698cca9bb73997a08dc2205
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-04-17 15:48:15 +0300

    test

--- index.php
+++ index.php
@@ -8,7 +8,15 @@ get_header();
 // endwhile;
 ?>
 <section class="homepage">
-    <h1></h1>
+    <div class="container">
+        <div class="row">
+            <?php for($i=0; i<12; i++){ ?>
+                <div class="col-4">
+                    <img src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/berry.jpg" alt="berry" class="w-100">
+                </div>
+            <?php } ?>
+        </div>
+    </div>
 </section>
 <script>
     jQuery(document).ready(function($) {
