e630d7fb464d71892a6b6ce4679929d1800cfcf7
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-04-18 13:05:09 +0300

    test

--- footer.php
+++ footer.php
@@ -9,8 +9,121 @@
  * @package WP_Bootstrap_Starter
  */
 ?>
-<footer>
-
+<footer class="pt-5">
+    <div class="container pt-5">
+        <div class="row justify-content-between pb-4">
+            <div class="col-2">
+                <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/Naqd-logo-white.png" alt="naqd">
+            </div>
+            <div class="col-5 justify-content-center d-flex align-items-center">
+                <ul class="social-media-icons">
+                    <li class="mx-1">
+                        <a href="#" class="single-social-icon">
+                            <img class="active" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/insta-icon.svg" alt="insta">
+                            <img class="stroke" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/insta.svg" alt="insta">
+                        </a>
+                    </li>
+                    <li class="mx-1">
+                        <a href="#" class="single-social-icon">
+                            <img class="active" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/fb-icon.svg" alt="fb">
+                            <img class="stroke" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/fb.svg" alt="fb">
+                        </a>
+                    </li>
+                    <li class="mx-1">
+                        <a href="#" class="single-social-icon">
+                            <img class="active" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/linkedin-icon.svg" alt="linkedin">
+                            <img class="stroke" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/in.svg" alt="linkedin">
+                        </a>
+                    </li>
+                    <li class="mx-1">
+                        <a href="#" class="single-social-icon">
+                            <img class="active" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/RSS-icon.svg" alt="RSS">
+                            <img class="stroke" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/rss.svg" alt="RSS">
+                        </a>
+                    </li>
+                    <li class="mx-1">
+                        <a href="#" class="single-social-icon">
+                            <img class="active" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/threads-icon.svg" alt="threads">
+                            <img class="stroke" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/threads.svg" alt="threads">
+                        </a>
+                    </li>
+                    <li class="mx-1">
+                        <a href="#" class="single-social-icon">
+                            <img class="active" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/tiktok-icon.svg" alt="tiktok">
+                            <img class="stroke" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/tiktok.svg" alt="tiktok">
+                        </a>
+                    </li>
+                    <li class="mx-1">
+                        <a href="#" class="single-social-icon">
+                            <img class="active" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/whatsapp-icon.svg" alt="whatsapp">
+                            <img class="stroke" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/whatsapp.svg" alt="whatsapp">
+                        </a>
+                    </li>
+                    <li class="mx-1">
+                        <a href="#" class="single-social-icon">
+                            <img class="active" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/X-icon.svg" alt="X">
+                            <img class="stroke" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/x.svg" alt="X">
+                        </a>
+                    </li>
+                    <li class="mx-1">
+                        <a href="#" class="single-social-icon">
+                            <img class="active" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/youtube-icon.svg" alt="youtube">
+                            <img class="stroke" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/youtube.svg" alt="youtube">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row pb-3">
+            <div class="col-12">
+                <ul class="footer-tags">
+                    <li>
+                        <a href="#">
+                            أفلامنا
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            ملفات
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            مجتمع
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            شاهد
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            اكتبوا معنا
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            عن نقد
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            اتصلوا بنا
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12">
+                <div class="d-flex justify-content-start text-right">
+                    <p>تابعوا قناة نقد على يوتيوب</p>
+                    <a class="button-green" href="#">
+                        الاشتراك
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
 </div><!-- #content -->
 </div><!-- #page -->
