dc87016b64edce37d505ef09ed2b8844605aae24
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-05-04 16:27:11 +0300

    about and contact us

--- about-us.php
+++ about-us.php
@@ -6,8 +6,8 @@ get_header();
 ?>
 <section class="about-and-contact-us py-5">
     <div class="container">
-        <div class="row">
-            <div class="col-12">
+        <div class="row justify-content-center">
+            <div class="col-lg-10 col-12">
                 <?php
                     while ( have_posts() ) : the_post();
                         the_content();
