8b2e824d1161149ae3e2a8b6ee7b2816882b3d14
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-05-04 16:25:10 +0300

    about and contact us

--- about-us.php
+++ about-us.php
@@ -4,7 +4,7 @@
  */
 get_header();
 ?>
-<section class="about-us py-5">
+<section class="about-and-contact-us py-5">
     <div class="container">
         <div class="row">
             <div class="col-12">
