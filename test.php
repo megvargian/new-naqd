ffdcc036c9e563fc1e75226c2e24bf87cde45bae
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-05-06 20:26:19 +0300

    test

--- test.php
+++ test.php
@@ -5,11 +5,11 @@
 get_header();
 ?>
 <!-- Button to Open Dialog -->
-<button onclick="openPopup()">Watch Short</button>
+<button class="openPopup">Watch Short</button>
 <!-- Overlay and Popup -->
 <div id="videoOverlay" class="overlay">
     <div class="position-relative w-100 h-100">
-        <button class="close-btn" onclick="closePopup()">
+        <button class="close-btn">
             <span aria-hidden="true">&times;</span>
         </button>
         <div class="popup">
@@ -22,18 +22,14 @@ get_header();
     </div>
 </div>
 <script>
-    function openPopup() {
-      document.getElementById('videoOverlay').style.display = 'block';
-    }
-
-    function closePopup() {
-      const overlay = document.getElementById('videoOverlay');
-      overlay.style.display = 'none';
-
-      // Stop the video by resetting the iframe
-      const iframe = overlay.querySelector('iframe');
-      iframe.src = iframe.src;
-    }
+    jQuery(document).ready(function($) {
+        $('.openPopup').click(function(){
+            $('#videoOverlay').style('display', 'block');
+        })
+        $('.close-btn').click(function(){
+            $('#videoOverlay').style('display', 'none');
+        })
+    });
 </script>
 <?php
 get_footer();
