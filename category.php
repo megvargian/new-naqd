ddee35af70a6783aed70cb1673acadd7fe96f4ba
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-05-07 13:49:42 +0300

    add category page

--- category.php
+++ category.php
@@ -3,6 +3,115 @@ get_header();
 ?>
 <section class="categories">
     <div class="container">
+        <div class="row">
+            <?php for($i=0; $i<8; $i++){ ?>
+                <div class="col-lg-3 col-12 mb-2 px-1">
+                    <div class="openPopup" data-key="<?php echo $i; ?>">
+                        <img class="w-100 d-block single-article-video" style="cursor: pointer;" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/berry.jpg" alt="berry">
+                        <img class="arrow-play" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/play.ico" alt="play">
+                    </div>
+                    <div class="overlay videoOverlay-<?php echo $i; ?>">
+                        <div class="position-relative w-100 h-100">
+                            <div class="popup">
+                                <button class="close-btn" data-key="<?php echo $i; ?>">
+                                    <span aria-hidden="true">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#fff"><path d="M.293.293a1 1 0 0 1 1.414 0L8 6.586 14.293.293a1 1 0 1 1 1.414 1.414L9.414 8l6.293 6.293a1 1 0 0 1-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L6.586 8 .293 1.707a1 1 0 0 1 0-1.414z"/></svg>
+                                    </span>
+                                </button>
+                                <iframe
+                                        frameborder="0"
+                                        width="360" height="640"
+                                        allowfullscreen
+                                        allow="autoplay; encrypted-media">
+                                </iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 col-12 mb-2 px-1">
+                <div class="most-read-articles">
+                    <h2 class="mb-3">
+                        الأكثر قراءة
+                    </h2>
+                    <ul>
+                        <?php for($i=0; $i<3; $i++){ ?>
+                            <li>
+                                <h3>
+                                    عن التروما التي تعشقنا وتحاول قتلنا
+                                </h3>
+                                <div class="author">
+                                    <a href="#">
+                                        متري طاهر - كاتب لبناني
+                                    </a>
+                                    <div class="date">
+                                        4 jan 2025
+                                    </div>
+                                </div>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-6 col-12">
+                <div class="row">
+                    <?php for($i=0; $i<2; $i++){ ?>
+                        <div class="col-lg-6 col-12 mb-2 px-1">
+                            <div class="openPopup" data-key="<?php echo $i; ?>">
+                                <img class="w-100 d-block single-article-video" style="cursor: pointer;" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/berry.jpg" alt="berry">
+                                <img class="arrow-play" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/play.ico" alt="play">
+                            </div>
+                            <div class="overlay videoOverlay-<?php echo $i; ?>">
+                                <div class="position-relative w-100 h-100">
+                                    <div class="popup">
+                                        <button class="close-btn" data-key="<?php echo $i; ?>">
+                                            <span aria-hidden="true">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#fff"><path d="M.293.293a1 1 0 0 1 1.414 0L8 6.586 14.293.293a1 1 0 1 1 1.414 1.414L9.414 8l6.293 6.293a1 1 0 0 1-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L6.586 8 .293 1.707a1 1 0 0 1 0-1.414z"/></svg>
+                                            </span>
+                                        </button>
+                                        <iframe
+                                                frameborder="0"
+                                                width="360" height="640"
+                                                allowfullscreen
+                                                allow="autoplay; encrypted-media">
+                                        </iframe>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <?php for($i=0; $i<4; $i++){ ?>
+                <div class="col-lg-3 col-12 mb-2 px-1">
+                    <div class="openPopup" data-key="<?php echo $i; ?>">
+                        <img class="w-100 d-block single-article-video" style="cursor: pointer;" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/berry.jpg" alt="berry">
+                        <img class="arrow-play" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/play.ico" alt="play">
+                    </div>
+                    <div class="overlay videoOverlay-<?php echo $i; ?>">
+                        <div class="position-relative w-100 h-100">
+                            <div class="popup">
+                                <button class="close-btn" data-key="<?php echo $i; ?>">
+                                    <span aria-hidden="true">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#fff"><path d="M.293.293a1 1 0 0 1 1.414 0L8 6.586 14.293.293a1 1 0 1 1 1.414 1.414L9.414 8l6.293 6.293a1 1 0 0 1-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L6.586 8 .293 1.707a1 1 0 0 1 0-1.414z"/></svg>
+                                    </span>
+                                </button>
+                                <iframe
+                                        frameborder="0"
+                                        width="360" height="640"
+                                        allowfullscreen
+                                        allow="autoplay; encrypted-media">
+                                </iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
         <div class="row pt-3">
                 <div class="col-12 mb-3 text-right">
                     <h2>
@@ -59,6 +168,26 @@ get_header();
 			}
         });
         swiper.changeLanguageDirection('rtl');
+        const youtubeShortslinks = [
+            'https://www.youtube.com/embed/UqI3exV3YPM?autoplay=1',
+            'https://www.youtube.com/embed/kx2_lOSBQ-Y?autoplay=1',
+            'https://www.youtube.com/embed/bCpk5aFgVtg?autoplay=1',
+            'https://www.youtube.com/embed/5LfiXvthTBA?autoplay=1',
+            'https://www.youtube.com/embed/UqI3exV3YPM?autoplay=1',
+            'https://www.youtube.com/embed/kx2_lOSBQ-Y?autoplay=1',
+            'https://www.youtube.com/embed/bCpk5aFgVtg?autoplay=1',
+            'https://www.youtube.com/embed/5LfiXvthTBA?autoplay=1',
+        ]
+        $('.openPopup').click(function(){
+            let key = $(this).attr('data-key');
+            $('.videoOverlay-' + key).css('display', 'block');
+            $('.videoOverlay-' + key).find('iframe').attr('src', youtubeShortslinks[key]);
+        })
+        $('.close-btn').click(function(){
+            var key = $(this).attr('data-key');
+            $('.videoOverlay-' + key).css('display', 'none');
+            $('.videoOverlay-' + key).find('iframe').attr('src', '');
+        })
     });
 </script>
 <?php
