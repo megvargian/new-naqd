8579bd8670820f1360a672ceac3787071e00ed4a
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-05-25 00:00:48 +0300

    add athors

--- header.php
+++ header.php
@@ -391,7 +391,7 @@ jQuery(document).ready(function($) {
 	<?php }?>
 	// toggle theme
 	$('.theme-toggle').click(function(event) {
-		$('body').toggleClass('light-theme');
+		$('html, body').toggleClass('light-theme');
 	});
 });
 </script>
