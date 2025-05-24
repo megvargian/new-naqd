3993485ccff3f360712c4110567bb219df44cb3f
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-05-25 00:47:09 +0300

    add athors

--- header.php
+++ header.php
@@ -317,7 +317,7 @@ $social_media_links = $get_general_fields['social_links'];
 jQuery(document).ready(function($) {
 	// toggle theme
 	$('#toggle-change').click(function(event) {
-		$('body').toggleClass('light-theme');
+		$('body').addClass('light-theme');
 	});
 	$('.menu_mobile_nav').click(function(event) {
 		$(this).toggleClass('active');
