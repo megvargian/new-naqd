334c98f057cb6ac2f25e397a3ca82d79db220f9e
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-05-24 23:36:10 +0300

    add athors

--- header.php
+++ header.php
@@ -18,6 +18,7 @@ $social_media_links = $get_general_fields['social_links'];
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/theme-toggles@4.10.1/css/classic.min.css">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <?php wp_head(); ?>
 	<style>
@@ -142,6 +143,37 @@ $social_media_links = $get_general_fields['social_links'];
 								</div>
 							</div>
 						</label> -->
+						<label class="theme-toggle" title="Toggle theme">
+							<input type="checkbox" />
+							<span class="theme-toggle-sr">Toggle theme</span>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								aria-hidden="true"
+								width="1em"
+								height="1em"
+								fill="currentColor"
+								stroke-linecap="round"
+								class="theme-toggle__classic"
+								viewBox="0 0 32 32"
+							>
+								<clipPath id="theme-toggle__classic__cutout">
+								<path d="M0-5h30a1 1 0 0 0 9 13v24H0Z" />
+								</clipPath>
+								<g clip-path="url(#theme-toggle__classic__cutout)">
+								<circle cx="16" cy="16" r="9.34" />
+								<g stroke="currentColor" stroke-width="1.5">
+									<path d="M16 5.5v-4" />
+									<path d="M16 30.5v-4" />
+									<path d="M1.5 16h4" />
+									<path d="M26.5 16h4" />
+									<path d="m23.4 8.6 2.8-2.8" />
+									<path d="m5.7 26.3 2.9-2.9" />
+									<path d="m5.8 5.8 2.8 2.8" />
+									<path d="m23.4 23.4 2.9 2.9" />
+								</g>
+								</g>
+							</svg>
+						</label>
 						<label class="switch">
 							<input type="checkbox" id="changeTheme">
 							<span class="slider">
