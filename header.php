3cdccd81b25ae9bb8af7bca5c4e58cea5b3a0d29
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-05-31 15:56:21 +0300

    test

--- header.php
+++ header.php
@@ -52,7 +52,7 @@ $length_cats_array = count($all_categories);
 		<?php } ?>
 		<?php
 			foreach ( $all_tags as $key => $tag ) {
-				$tag_count = $length_cats_array + 2;
+				$tag_count = $length_cats_array + 1;
 		?>
 			.search-articles .filter-list li:nth-child(<?php echo $tag_count; ?>) button:hover{
 				border-color: <?php echo get_field('border_color', 'post_tag_' . $tag->term_id);?>;
@@ -60,7 +60,8 @@ $length_cats_array = count($all_categories);
 			.search-articles .filter-list li:nth-child(<?php echo $tag_count; ?>) button.active{
 				border-color: <?php echo get_field('border_color', 'post_tag_' . $tag->term_id);?>;
 			}
-		<?php } ?>
+		<?php $tag_count++;
+		} ?>
 	</style>
 </head>
 <body <?php body_class(); ?>>
