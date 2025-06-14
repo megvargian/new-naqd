238e48602747957edb572198fc8890f739f7f974
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-06-14 21:21:47 +0300

    test

--- index.php
+++ index.php
@@ -403,8 +403,10 @@ $top_posts = get_top_3_most_visited('post');
         var parentWidth = $('.parent-row-rassif').width();
         var parentHeight = $('.parent-row-rassif').height();
         $('.rassif-section').hover(function() {
-            $(this).width(parentWidth);
-            $(this).height(parentHeight);
+            $(this).css('width', ''+parentWidth+' px !important;');
+            $(this).css('height', ''+parentHeight+' px !important;');
+            console.log(parentHeight);
+            console.log(parentWidth);
         });
         }
     })
