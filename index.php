a744299a73b8a0b0a85a821e15f958aadcd3d74a
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-06-19 15:26:27 +0300

    test

--- index.php
+++ index.php
@@ -51,8 +51,8 @@ $top_posts = get_top_3_most_visited('post');
                 $limited = implode( ' ', array_slice( $words, 0, 200 ) );
         ?>
             <div class="row bg-color-green mb-2"  <?php if(!isMob()) {?>style="background-color: <?php echo $get_homepage_fields['background_color']; ?>" <?php } ?>>
-                <div class="col-lg-4 col-12 mb-2 mb-sm-0 px-1 px-sm-0">
-                    <a href="<?php echo get_permalink($article_id); ?>" class="position-relative">
+                <div class="col-lg-4 col-12 mb-2 mb-sm-0 px-1 px-sm-0 position-relative">
+                    <a href="<?php echo get_permalink($article_id); ?>">
                         <img class="w-100 h-100 d-block main-img" src="<?php echo $image_url; ?>" alt="<?php echo $article_title; ?>">
                         <img class="feature" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/feature-dark.png" alt="feature">
                     </a>
