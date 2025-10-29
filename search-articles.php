a4f5e6a98a8ce512d5b4a0d2d0d3d5a0b845114b
Megvargian123 <mikekouyoumdjian@hotmail.com>
2025-10-29 13:41:01 +0200

    fix

--- search-articles.php
+++ search-articles.php
@@ -7,7 +7,7 @@ if(isset($_GET['searchArticle'])){
     $search_article = $_GET['searchArticle'];
 }
 $args = array(
-    'post_type'      => 'post',
+    'post_type'      => array('post', 'video'),
     'posts_per_page' =>  8,
     'orderby'        => 'date',
     'order'          => 'DESC',
@@ -87,14 +87,47 @@ $tags = $get_general_fields['tags'];
                             $article_id = get_the_ID();
                             $article_title = get_the_title($article_id);
                             $image_url = get_the_post_thumbnail_url($article_id);
+                            $post_type = get_post_type($article_id);
                             $count++;
+
+                            // Check if it's a video
+                            if ($post_type === 'video') {
+                                $url = get_field('youtube_url', $article_id);
+                                $path = parse_url($url, PHP_URL_PATH);
+                                $parts = explode('/', $path);
+                                $video_embed_id = end($parts);
                     ?>
+                        <div class="col-lg-3 col-12 mb-2 px-1">
+                            <div class="openPopup <?php echo $count > 8 ? 'fade-in' : ''?>" data-key="<?php echo $article_id; ?>" data-key-url="<?php echo $video_embed_id; ?>">
+                                <img class="w-100 d-block single-article-video" style="cursor: pointer;" src="<?php echo $image_url;?>" alt="<?php echo $article_title;?>">
+                                <img class="arrow-play" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/play.ico" alt="play">
+                            </div>
+                            <div class="overlay videoOverlay-<?php echo $article_id; ?>">
+                                <div class="position-relative w-100 h-100">
+                                    <div class="popup">
+                                        <button class="close-btn" data-key="<?php echo $article_id; ?>">
+                                            <span aria-hidden="true">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#fff"><path d="M.293.293a1 1 0 0 1 1.414 0L8 6.586 14.293.293a1 1 0 1 1 1.414 1.414L9.414 8l6.293 6.293a1 1 0 0 1-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L6.586 8 .293 1.707a1 1 0 0 1 0-1.414z"/></svg>
+                                            </span>
+                                        </button>
+                                        <iframe
+                                                frameborder="0"
+                                                width="360" height="640"
+                                                allowfullscreen
+                                                allow="autoplay; encrypted-media">
+                                        </iframe>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } else { // Regular post ?>
                         <div class="col-lg-3 col-12 mb-2 px-1">
                             <a href="<?php echo get_permalink($article_id);?>" class="<?php echo $count > 8 ? 'fade-in' : ''?>">
                                 <img class="w-100 d-block single-article " src="<?php echo $image_url; ?>" alt="<?php echo $article_title; ?>">
                             </a>
                         </div>
                     <?php
+                            }
                         }
                         wp_reset_postdata();
                     }
@@ -214,6 +247,43 @@ $tags = $get_general_fields['tags'];
         $('#load-more-button').click(function() {
             loadMorePosts();
         });
+
+        // Video popup functionality
+        $(document).on('click', '.openPopup', function(){
+            let key = $(this).attr('data-key');
+            let embedKey = $(this).attr('data-key-url');
+            <?php if(isMob()){ ?>
+                window.location.href = 'https://www.youtube.com/embed/'+embedKey+'?autoplay=1';
+            <?php } else { ?>
+                $('.videoOverlay-' + key).css('display', 'block');
+                $('.videoOverlay-' + key).find('iframe').attr('src', 'https://www.youtube.com/embed/'+embedKey+'?autoplay=1');
+		        $('html, body').addClass('hide_scroll');
+            <?php } ?>
+            //add counter in db
+            addCounterViewForVideo(key);
+        });
+
+        $(document).on('click', '.close-btn', function(){
+            var key = $(this).attr('data-key');
+            $('.videoOverlay-' + key).css('display', 'none');
+            $('.videoOverlay-' + key).find('iframe').attr('src', '');
+		    $('html, body').removeClass('hide_scroll');
+        });
+
+        // Function to add video view counter
+        function addCounterViewForVideo(id) {
+            $.ajax({
+                type: 'POST',
+                url: '<?php echo admin_url('admin-ajax.php'); ?>',
+                data: {
+                    action: 'add_counter_view_video',
+                    id: id
+                },
+                success: function(response) {
+                    console.log('Video view counted');
+                }
+            });
+        }
     });
 </script>
 <?php
